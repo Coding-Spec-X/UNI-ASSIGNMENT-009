1b722740c3a7462238e3833994145bce4bb8ac65
Zac <Zac@RIPPLE-XRP>
2022-05-15 18:38:33 +1200

    Add project files.

---
+++ Practical/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Practical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.displayMenu = new System.Windows.Forms.MenuStrip();
+            this.displayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawPlannerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numHoursTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.displayMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(16, 71);
+            this.pictureBoxDisplay.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(600, 600);
+            this.pictureBoxDisplay.TabIndex = 1;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // displayMenu
+            // 
+            this.displayMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.displayMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.displayToolStripMenuItem});
+            this.displayMenu.Location = new System.Drawing.Point(0, 0);
+            this.displayMenu.Name = "displayMenu";
+            this.displayMenu.Size = new System.Drawing.Size(807, 30);
+            this.displayMenu.TabIndex = 2;
+            this.displayMenu.Text = "Display";
+            // 
+            // displayToolStripMenuItem
+            // 
+            this.displayToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.drawPlannerToolStripMenuItem,
+            this.clearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.displayToolStripMenuItem.Name = "displayToolStripMenuItem";
+            this.displayToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.displayToolStripMenuItem.Text = "Display";
+            // 
+            // drawPlannerToolStripMenuItem
+            // 
+            this.drawPlannerToolStripMenuItem.Name = "drawPlannerToolStripMenuItem";
+            this.drawPlannerToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.D)));
+            this.drawPlannerToolStripMenuItem.Size = new System.Drawing.Size(273, 26);
+            this.drawPlannerToolStripMenuItem.Text = "Draw Planner";
+            this.drawPlannerToolStripMenuItem.Click += new System.EventHandler(this.drawPlannerToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(273, 26);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(273, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(668, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of Hours:";
+            // 
+            // numHoursTextBox
+            // 
+            this.numHoursTextBox.Location = new System.Drawing.Point(671, 103);
+            this.numHoursTextBox.Name = "numHoursTextBox";
+            this.numHoursTextBox.Size = new System.Drawing.Size(100, 22);
+            this.numHoursTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 703);
+            this.Controls.Add(this.numHoursTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.displayMenu);
+            this.MainMenuStrip = this.displayMenu;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.displayMenu.ResumeLayout(false);
+            this.displayMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.MenuStrip displayMenu;
+        private System.Windows.Forms.ToolStripMenuItem displayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawPlannerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox numHoursTextBox;
+    }
+}
+
---
+++ Practical/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical
+{
+    public partial class Form1 : Form
+    {
+        //The minimum number of hours to display
+        const int MIN_HOURS = 1;
+        //The maximum number of hours to display
+        const int MAX_HOURS = 24;
+        //The number of days to display
+        const int NUM_DAYS = 7;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //METHOD TO DRAW SQUARE
+        private void DrawSquare(int x, int y, int apptWidth, int apptHeight, Graphics paper, Color bColor, Color pColor)
+        {
+            SolidBrush br = new SolidBrush(bColor);
+            paper.FillRectangle(br, x, y, apptWidth, apptHeight);
+            Pen pen1 = new Pen(pColor, 2);
+            paper.DrawRectangle(pen1, x, y, apptWidth, apptHeight);
+        }
+        //METHOD TO DRAW ROW
+        private void DrawRow(int y, int sqWidth, int sqHeight, Graphics paper)
+        {
+            int x = 0;
+            for (int day = 0; day <= NUM_DAYS; day++)
+            {
+                if (day >= 5)
+                {
+                    DrawSquare(x, y, sqWidth, sqHeight, paper, Color.LightGreen, Color.Black);
+                }
+                else
+                {
+                    DrawSquare(x, y, sqWidth, sqHeight, paper, Color.LightBlue, Color.Black);
+                }
+                x += sqWidth;
+            }
+        }
+        private void drawPlannerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //declare variable for textbox
+            int hours = int.Parse(numHoursTextBox.Text);
+            try
+            {
+                int xPos = 0;
+                int yPos = 0;
+                int squareWidth = pictureBoxDisplay.Width / NUM_DAYS;
+                int squareHeight = pictureBoxDisplay.Height / hours;
+                Graphics canvas = pictureBoxDisplay.CreateGraphics();
+
+                //Display error message for incorrect values
+                if (hours < MIN_HOURS || hours > MAX_HOURS)
+                {
+                    MessageBox.Show("Oops! Looks like you have entered in a wrong amount. Please enter a value between 2 and 24.");
+                }
+                else
+                {
+                    //set row and column length then allocate colors to specific columns
+                    for (int row = 1; row <= hours; row++)
+                    {
+                        for (int col = 1; col <= NUM_DAYS; col++)
+                        {
+                            if (col == 6 && col == 7)
+                            {
+                                DrawSquare(xPos, yPos, squareWidth, squareHeight, canvas, Color.LightGreen, Color.Black);
+                            }
+                            else
+                            {
+                                DrawSquare(xPos, yPos, squareWidth, squareHeight, canvas, Color.LightBlue, Color.Black);
+                            }
+                            DrawRow(yPos, squareWidth, squareHeight, canvas);
+                        }
+                        yPos += squareHeight;
+                        xPos = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBoxDisplay.Refresh();
+        }
+    }
+}
+
+
